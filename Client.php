01c8049cc28f00129694d713abb0c143f9dac6a4
Kenan Mursalov <kenan.mursalov@gmail.com>
2022-04-07 12:04:51 +0300

    changed clients.php and index.php

--- Client.php
+++ Client.php
@@ -4,5 +4,68 @@ namespace app;
 
 class Client
 {
+    private $firstName;
+    private $lastMame;
+    private $phone;
+
+    /**
+     * @param $firstName
+     * @param $lastMame
+     * @param $phone
+     */
+    public function __construct($firstName, $lastMame, $phone)
+    {
+        $this->firstName = $firstName;
+        $this->lastMame = $lastMame;
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastMame(): string
+    {
+        return $this->lastMame;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName(mixed $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @param mixed $lastMame
+     */
+    public function setLastMame(mixed $lastMame): void
+    {
+        $this->lastMame = $lastMame;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone(mixed $phone): void
+    {
+        $this->phone = $phone;
+    }
 
 }
--- index.php
+++ index.php
@@ -1,4 +1,2 @@
 <?php
-phpinfo();
-?>
-        ``
+echo 'Hello git';
