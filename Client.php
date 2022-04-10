1791344da3366d9d0150f0656187e362121b25a0
Kenan Mursalov <kenan.mursalov@gmail.com>
2022-04-10 18:14:32 +0300

    Changed Client.php

--- Client.php
+++ Client.php
@@ -8,62 +8,10 @@ class Client
     private $lastMame;
     private $phone;
 
-    /**
-     * @param $firstName
-     * @param $lastMame
-     * @param $phone
-     */
-    public function __construct($firstName, $lastMame, $phone)
+    public function __construct($firstName, $lastName, $phone)
     {
         $this->firstName = $firstName;
-        $this->lastMame = $lastMame;
-        $this->phone = $phone;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getFirstName(): string
-    {
-        return $this->firstName;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getLastMame(): string
-    {
-        return $this->lastMame;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPhone(): string
-    {
-        return $this->phone;
-    }
-
-    /**
-     * @param mixed $firstName
-     */
-    public function setFirstName(mixed $firstName): void
-    {
-        $this->firstName = $firstName;
-    }
-
-    /**
-     * @param mixed $lastMame
-     */
-    public function setLastMame(mixed $lastMame): void
-    {
-        $this->lastMame = $lastMame;
-    }
-    /**
-     * @param mixed $phone
-     */
-    public function setPhone(mixed $phone): void
-    {
+        $this->lastMame = $lastName;
         $this->phone = $phone;
     }
 
